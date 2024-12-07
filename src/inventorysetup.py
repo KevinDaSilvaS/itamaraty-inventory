f62d80b73884fc244a622dd8f0b054b2905bcd9a
Kevin <kevin.kds80@gmail.com>
2024-12-07 13:25:42 -0300

    add csv parsing to Item

---
+++ src/inventorysetup.py
@@ -0,0 +1,23 @@
+def parse_inventory_csv(model):
+    f = open("inventory.csv", "r")
+    f.readline() #discarding keys
+
+    items = []
+    while (True):
+        line = f.readline()
+        if (line == ''):
+            break
+        line = line.split(';')
+        parsed_item = model(
+            section=line[0],
+            location=line[1],
+            archive_number=line[2],
+            amount=line[5],
+            description=line[6],
+            origin=line[10],
+            conservation_status=line[13],
+            obs=line[14]
+        )
+        items.append(parsed_item)
+
+    return items
